f2e080939e41b1168ce6316cbeee8c73343a356a
pnada <melove.mariokart@gmail.com>
2024-08-05 15:11:36 +1000

    Fixed previous issues. Now creates a big folder but is usable!

--- CJ_deepfri_from_seq.py
+++ CJ_deepfri_from_seq.py
@@ -9,5 +9,5 @@ if pathExist == 0:
 seq2pdbchain = f"python seq2pdbchain.py -s {args.sequence} > './results/{args.output}/{args.output}.pdb'"
 subprocess.run(seq2pdbchain, shell=True)
 
-pdb2deepfri = f"python ./Deepfri/predict.py -pdb './results/{args.output}/{args.output}.pdb' -ont mf bp cc ec -v -o './results/{args.output}/{args.output}' {args.additional_args}"
+pdb2deepfri = f"python predict.py -pdb './results/{args.output}/{args.output}.pdb' -ont mf bp cc ec -v -o './results/{args.output}/{args.output}' {args.additional_args}"
 subprocess.run(pdb2deepfri, shell=True)
